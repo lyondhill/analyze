be9ac3f791bb9e3b59ea45900531f94b8aba250d
Lyon <lyon@delorum.com>
2011-11-16 10:32:52 -0700

    new stuff

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var sys  = require("sys");
 var odbc = require("odbc");
 
 var db = new odbc.Database();
-db.open("DRIVER={MonetDB};SERVER=localhost;UID=monetdb;PWD=monetdb;DATABASE=my-first-db", function(err)
+db.open("DRIVER={libmonetdb5};Server=localhost;Port=50000;UID=monetdb;PWD=monetdb;DATABASE=my-first-db", function(err)
 {
     db.query("select * from table", function(err, rows, moreResultSets)
     {
