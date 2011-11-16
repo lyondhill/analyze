326b09a0601b34f6a5004961d7891cf78848d496
Lyon <lyon@delorum.com>
2011-11-16 10:39:22 -0700

    newstuff

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
-var sys  = require("sys");
 var odbc = require("odbc");
+console.log("start")
 
 var db = new odbc.Database();
+console.log(db)
 db.open("DRIVER={monetdb5};Server=localhost;Port=50000;UID=monetdb;PWD=monetdb;DATABASE=my-first-db", function(err)
 {
     db.query("select * from table", function(err, rows, moreResultSets)
     {
-        sys.debug(sys.inspect(rows));
+        console.log(rows);
         db.close(function(){});
     });
 });
