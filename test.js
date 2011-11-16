5a478069b44b4be55e35fc5b43241a9bf71483e1
Lyon <lyon@delorum.com>
2011-11-16 11:37:30 -0700

    add new

--- test.js
+++ test.js
@@ -5,6 +5,9 @@ var db = new odbc.Database();
 console.log(db)
 db.open("DRIVER={MonetDB};Server=localhost;Port=50000;UID=monetdb;PWD=monetdb;DATABASE=my-first-db", function(err)
 {
+	db.query("INSERT INTO lyon_farts VALUES ('sbd', 2, 5);")
+	db.query("INSERT INTO lyon_farts VALUES ('pop', 1, 2);")
+	db.query("INSERT INTO lyon_farts VALUES ('shard', 3, 10);")
     db.query("SELECT * FROM lyon_farts WHERE severity > 3;", function(err, rows, moreResultSets)
     {
         console.log(rows);
