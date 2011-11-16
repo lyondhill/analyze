d21d31d3bb3776f5e35bb287cc98fc53133f52a5
Lyon <lyon@delorum.com>
2011-11-16 11:34:59 -0700

    aweasdf

--- test.js
+++ test.js
@@ -3,9 +3,9 @@ console.log("start")
 
 var db = new odbc.Database();
 console.log(db)
-db.open("DRIVER={monetdb5};Server=localhost;Port=50000;UID=monetdb;PWD=monetdb;DATABASE=my-first-db", function(err)
+db.open("DRIVER={MonetDB};Server=localhost;Port=50000;UID=monetdb;PWD=monetdb;DATABASE=my-first-db", function(err)
 {
-    db.query("select * from table", function(err, rows, moreResultSets)
+    db.query("SELECT * FROM lyon_farts WHERE severity > 3;", function(err, rows, moreResultSets)
     {
         console.log(rows);
         db.close(function(){});
